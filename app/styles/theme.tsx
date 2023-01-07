a8a9a5f098c0456353044f8af6b84ba6a2c4ec7c
Antony-Lester <me@antonylester.com>
2023-01-07 21:41:56 +0000

    added inital themes

---
+++ app/styles/theme.tsx
@@ -0,0 +1,36 @@
+interface Theme {
+    mainLight: string;
+    main: string;
+    mainDark: string;
+    mainText: string;
+
+    subLight: string;
+    sub: string;
+    subDark: string;
+    subText: string;
+}
+
+export const light : Theme = {
+    mainLight: '#eeffff',
+    main: '#bbdefb',
+    mainDark: '#8aacc8',
+    mainText: '#000000',
+
+    subLight: '#819ca9',
+    sub: '#546e7a',
+    subDark: '#29434e',
+    subText: '#ffffff',
+}
+export const dark : Theme = {
+    mainLight: '#484848',
+    main: '#212121',
+    mainDark: '#000000',
+    mainText: '#ffffff',
+
+    subLight: '#4f5b62',
+    sub: '#263238',
+    subDark: '#000a12',
+    subText: '#ffffff',
+}
+
+//might need to add: background, surface & error
